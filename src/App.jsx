0762fda97a3f6c278eeec7f96de7b0d8c78a3d6f
symonmuchemi <simonkenya846@gmail.com>
2024-01-30 00:24:53 +0300

    used useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
-	const [advice, setAdvice] = useState("")
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
 
-	async function getAdvice() {
-		const res = await fetch('https://api.adviceslip.com/advice')
-		const data  = await res.json()
-
-		setAdvice(data.slip)
-		console.log(data)
-	}
-	return <div>
-		<h1>{advice.id}: {advice.advice}</h1>
-		<button onClick={getAdvice}>Get Advice</button>
-	</div>
+    setAdvice(data.slip);
+    setCount((cnt) => cnt + 1);
+    console.log(data);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+  return (
+    <div>
+      <h1>
+        {advice.id + ": " + advice.advice}
+      </h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice{" "}
+      </p>
+    </div>
+  );
 }
